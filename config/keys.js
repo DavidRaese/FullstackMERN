0aee80143cb57527872936c1bc2278e0d6d7f0df
David <david.raese@gmx.de>
2018-05-04 12:19:19 +0200

    initial commit - server setup - routes setup > users, profile, posts

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://david:pCLdfA_t1@ds151259.mlab.com:51259/mern_stack_test"
+};
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
+const port = process.env.PORT || 5000;
+//DB config
+const db = require("./config/keys").mongoURI;
+//connect to mongodb
+mongoose
+  .connect(db)
+  .then(() => console.log("mongodb connected"))
+  .catch(err => console.log(err));
+
+//use routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+app.get("/", (req, res) => res.send("hallo welt!!"));
+
+app.listen(port, () => console.log(`Server is listening on Port: ${port}`));
